6c5d95b7df0e3d99522a510f177ab45f2723fe84
Sergio <nakul.sv@gmail.com>
2018-12-15 21:55:39 +0200

    Add destruction after 1 hour

---
+++ config/initializers/scheduler.rb
@@ -0,0 +1,12 @@
+require 'rufus-scheduler'
+
+scheduler = Rufus::Scheduler::singleton
+
+scheduler.every '1m' do
+  @messages = Message.where(destruction: false).all
+  @messages.each do |message|
+    if Time.now - message.created_at > 1.hour
+      message.destroy
+    end
+  end
+end
