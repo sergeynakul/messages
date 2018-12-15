3ffc359ee4d2e56eb8ebbd4b339c3cd274293c8e
Sergio <nakul.sv@gmail.com>
2018-12-15 19:26:18 +0200

    Delete worker

--- app/workers/delete_worker.rb
+++
@@ -1,9 +0,0 @@
-class DeleteWorker
-  include Sidekiq::Worker
-
-  recurrence { minutely }
-
-  def perform
-    Message.where('created_at >= :thirty_days_ago', thiryty_days_ago: Time.now - 1.minute).destroy_all
-  end
-end
